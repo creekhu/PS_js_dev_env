a5deccfeb5155c608b4f41d3c924b66575c403c3
creekhu <creekthu@gmail.com>
2018-02-24 19:29:48 -0800

    Add Mock Data and API

---
+++ src/api/userApi.js
@@ -0,0 +1,34 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+console.log(baseUrl);
+
+export function getUsers() {
+	return get('users');
+}
+
+function get(url) {
+	return fetch(baseUrl + url)
+		.then(onSuccess, onError);
+}
+
+export function deleteUser(id) {
+	return del(`users/${id}`);
+}
+
+function del(url) {
+	const request = new Request(baseUrl + url, {
+		method: 'DELETE'
+	});
+
+	return fetch(request).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+	return response.json();
+}
+
+function onError(error) {
+	console.log(error); // eslint-disable-line no-console
+}
